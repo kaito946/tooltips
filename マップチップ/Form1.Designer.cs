797d2d4a678bca5f8a23262ed17a567fcaa60802
伊藤 海徒 <itokaito007@gmail.com>
2025-03-13 07:20:52 +0900

    プロジェクト ファイルを追加します。

---
+++ マップチップ/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace マップチップ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(228, 141);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(244, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            this.dataGridView1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseMove);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ マップチップ/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace マップチップ
+{
+
+    public partial class Form1 : Form
+    {
+        System.Windows.Forms.ToolTip toolTip = new System.Windows.Forms.ToolTip();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            dataGridView1.Rows.Add("A00123456789abcdefg", "商品A");
+            dataGridView1.Rows.Add("B002", "商品B");
+
+        }
+
+        
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            int targetColumnIndex = 0; // ずらしたい列のインデックス
+            int offsetX = 50; // 右にずらすピクセル数
+
+            if (e.RowIndex >= 0 && e.ColumnIndex == targetColumnIndex)
+            {
+                // 背景を描画
+                e.PaintBackground(e.ClipBounds, true);
+
+                // 文字列の描画領域を作成
+                Rectangle textRect = new Rectangle(
+                    e.CellBounds.X + offsetX, // 右にずらす
+                    e.CellBounds.Y,
+                    e.CellBounds.Width - offsetX,
+                    e.CellBounds.Height
+                );
+
+                // 文字列を描画
+                TextFormatFlags flags = TextFormatFlags.VerticalCenter | TextFormatFlags.Left | TextFormatFlags.EndEllipsis;
+                TextRenderer.DrawText(e.Graphics, e.FormattedValue?.ToString(), e.CellStyle.Font, textRect, e.CellStyle.ForeColor, flags);
+
+                e.Handled = true; // 既定の描画を抑制
+            }
+            else
+            {
+                e.Paint(e.ClipBounds, DataGridViewPaintParts.All);
+            }
+        }
+
+        private void dataGridView1_CellToolTipTextNeeded(object sender, DataGridViewCellToolTipTextNeededEventArgs e)
+        {
+
+        }
+
+
+        private void dataGridView1_MouseMove(object sender, MouseEventArgs e)
+        {
+            DataGridView.HitTestInfo hit = dataGridView1.HitTest(e.X, e.Y);
+
+            if (hit.Type == DataGridViewHitTestType.Cell && hit.RowIndex >= 0 && hit.ColumnIndex == 0)
+            {
+                DataGridViewCell cell = dataGridView1.Rows[hit.RowIndex].Cells[hit.ColumnIndex];
+                string cellText = cell.Value?.ToString();
+
+                if (!string.IsNullOrEmpty(cellText) && IsTextClipped(hit.RowIndex, hit.ColumnIndex, cellText))
+                {
+                    toolTip.Show(cellText, dataGridView1, e.X + 10, e.Y + 20, 2000); // 2秒表示
+                }
+                else
+                {
+                    toolTip.Hide(dataGridView1);
+                }
+            }
+            else
+            {
+                toolTip.Hide(dataGridView1);
+            }
+        }
+
+        private bool IsTextClipped(int rowIndex, int columnIndex, string text)
+        {
+            using (Graphics g = dataGridView1.CreateGraphics())
+            {
+                DataGridViewCell cell = dataGridView1.Rows[rowIndex].Cells[columnIndex];
+                Font font = cell.InheritedStyle.Font;
+                int cellWidth = cell.Size.Width - 50; // 右側の 16 ピクセルを考慮
+
+                // テキストの描画サイズを測定
+                Size textSize = TextRenderer.MeasureText(g, text, font);
+
+                // テキストがセルの幅より大きければ省略される
+                return textSize.Width > cellWidth;
+            }
+        }
+    }
+}
